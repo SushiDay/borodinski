7ae845129d8d035bc4b1d642dc06661bb6285e6f
SushiDay <sushi_day@yahoo.com>
2017-04-23 16:41:22 +0700

    map was added

--- script/main.js
+++ script/main.js
@@ -1,14 +1,38 @@
 var login = document.querySelector(".login");
 var popup = document.querySelector(".pop-up");
 var close = document.querySelector(".pop-up button");
-
+var buttons = document.querySelector(".right-column button");
+var mapopener = document.querySelector(".open-map");
+var map = document.querySelector(".map");
+var closeMap = document.querySelector(".map button");
 
 login.addEventListener("click", function(event) {
     event.preventDefault();
-    popup.classList.add("pop-up-shown", "fade");
+    popup.classList.add("pop-up-shown", "fall");
+    
+});
+
+mapopener.addEventListener("click", function(event) {
+    event.preventDefault();
+    map.classList.add("map-shown", "fade");
 });
 
 close.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.remove("pop-up-shown");
 });
+
+closeMap.addEventListener("click", function(event){
+    event.preventDefault();
+    map.classList.remove("map-shown");
+});
+
+window.addEventListener("keydown", function(event) {
+    event.preventDefault();
+    if(event.keyCode == 27) {
+        if(popup.classList.contains("pop-up-shown")) {
+            popup.classList.remove("pop-up-shown");
+        }        
+    }
+});
+
